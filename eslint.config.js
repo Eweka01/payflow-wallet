464257ae2776e015e8f6e7f4a00c1faf8001c104
Eweka01 <oseweka1@gmail.com>
2026-05-15 22:45:09 -0400

    Migrate ESLint config to flat config format (v9/v10)
    Workflow installs latest ESLint (v10) which dropped .eslintrc.json support.
    Replace with eslint.config.js using CommonJS flat config format.

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "off",
+    },
+  },
+  {
+    ignores: ["services/frontend/**", "**/node_modules/**", "**/coverage/**"],
+  },
+];
